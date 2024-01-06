7661d9cdd0e7db0cc8afd7fffeb7beb1c819d592
hesam hosseini <134384375+nuclearHesam@users.noreply.github.com>
2024-01-06 15:14:34 +0330

    Add project files.

---
+++ ModernDesing/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using ModernDesing.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernDesing.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        public HomeViewModel HomeVm { get; set; }
+        public DiscoveryViewModel DiscoveryVm { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVm = new();
+            DiscoveryVm = new();
+
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVm;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVm;
+            });
+        }
+    }
+}
